99413eff12429d76107e906a9f6c18c24878583c
TomtomPudding <tomtomhobby1125@gmail.com>
2021-09-05 01:20:00 +0900

    [update] mapping

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader};
 use serde::{Deserialize};
+use std::time::Instant;
 
 #[derive(Debug, Deserialize)]
 struct Record {
@@ -14,25 +14,35 @@ struct Record {
     state: String,
 }
 
-// https://qiita.com/algebroid/items/c456d4ec555ae04c7f92#%E3%83%87%E3%83%AA%E3%83%9F%E3%82%BF%E3%82%AF%E3%82%A9%E3%83%BC%E3%83%88%E3%81%9D%E3%81%97%E3%81%A6%E5%8F%AF%E5%A4%89%E9%95%B7%E3%83%AC%E3%82%B3%E3%83%BC%E3%83%89
-fn main() -> Result<(), Box<dyn std::error::Error>>  {
-    println!("start read csv");
-    for result in BufReader::new(File::open("csv/test.csv")?).lines() {
-        let ll = result?;
-        println!("{}", ll);
+fn read_csv() -> Vec<Record> {
+    let file = match File::open("csv/test.csv") {
+        Err(why) => panic!("couldn't open csv: {:?}", why),
+        Ok(file) => file,
+    };
+    let mut rdr = csv::ReaderBuilder::new()
+        .double_quote(false)
+        .escape(Some(b'$'))
+        .flexible(true)
+        .comment(Some(b'#'))
+        .from_reader(file);
+    let mut recode_vec: Vec<Record> = Vec::new();
+    for result in rdr.deserialize() {
+        match result {
+            Err(why) => {
+                println!("{:?}", why)
+            },
+            Ok(result) => recode_vec.push(result)
+        }
     }
     println!("end read csv");
 
-    let mut rdr = csv::ReaderBuilder::new()
-        .double_quote(false)
-        .escape(Some(b'\\'))
-        .flexible(true)
-        .comment(Some(b'#'))
-        .from_reader(File::open("csv/test.csv")?);
-    for result in rdr.deserialize() {
-        let record: Record = result?;
-        println!("{:?}", record);
-    }
-    println!("end read csv");
-    Ok(())
+    // println!("{:?}", recode_vec);
+    recode_vec
+}
+
+fn main() {
+    let start = Instant::now();
+    read_csv();
+    let duration = start.elapsed();
+    println!("Time elapsed in read_csv() is: {:?}", duration);
 }
